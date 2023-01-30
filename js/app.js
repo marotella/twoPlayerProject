a7bed1702820f99872534f2d10268554a4cccc95
marotella <111547566+marotella@users.noreply.github.com>
2023-01-30 15:57:52 -0500

    functions and event listners setup

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//create a basic class 
+//create a basic class to make characters 
 class fighter {
     constructor (name){
         this.name=name
@@ -26,11 +26,21 @@ function createPlayerOne(name) {
 
 }
 
-
+//create variables for each button
 const fighter1pic = document.querySelector(".player1pic")
 const fighter2pic =document.querySelector(".player2pic")
-
+const player1health = document.querySelector(".player1health")
+const player2health = document.querySelector(".player2health")
+const player1Attack = document.querySelector(".p1attack")
+const player1Special = document.querySelector(".p1special")
+const player1Restore = document.querySelector(".p1restore")
+const player2Attack = document.querySelector(".p2attack")
+const player2Special = document.querySelector(".p2special")
+const player2Restore = document.querySelector(".p2restore")
 //create event listeners based on clicks yhat will create the new characters.
+
+
+//Player one 
 const fighter1JCVDButton = document.querySelector(".jcvdButton1")
 console.log(fighter1JCVDButton)
 fighter1JCVDButton.addEventListener("click", function (){
@@ -44,7 +54,6 @@ fighter1CommandoButton.addEventListener("click",  function (){
     fighter1pic.setAttribute("src", "images/arnold.png")
 })
 
-
 const fighter1RamboButton = document.querySelector(".ramboButton1")
 console.log(fighter1RamboButton)
 fighter1RamboButton.addEventListener("click", function (){
@@ -52,8 +61,7 @@ fighter1RamboButton.addEventListener("click", function (){
     fighter1pic.setAttribute("src", "images/RAmbo.png")
 })
 
-
-
+//Player Two
 const fighter2JCVDButton = document.querySelector(".jcvdButton2")
 console.log(fighter2JCVDButton)
 fighter2JCVDButton.addEventListener("click", function (){
@@ -77,7 +85,7 @@ fighter2RamboButton.addEventListener("click",function (){
 console.log(fighter1)
 console.log(fighter2)
 
-
+//Monitors turn taking should post to innerHTML the turn and start on click
 let counter = 0
 const checkHealth = () => {
     if(fighter1.health == 0){
@@ -87,27 +95,34 @@ const checkHealth = () => {
         alert("Player 2 wins!")
         
     }else {
+        player1health.setAttribute("value", fighter1.health)
+        player2health.setAttribute("value", fighter2.health)
         counter = counter + 1
     }
 }
+//Monitors turn taking should post to innerHTML the turn and start on click
 
 const gameTurns = () =>{
     if(counter % 2 == 0){
         alert("It is player 1's turn")
-        checkHealth()
+    
     } else{
         alert("It is player 2's turn")
-        checkHealth()
+     
     }
 }
 
 
-//create formula for fighting options 
+//create functions for fighting options and event listeners
 
+
+//Player one actions
 const fighter1Attack = () => {
     if (Math.random(fighter1.accuracy)<=0.8){
         fighter2.health = fighter2.health - 15
-    }else {
+        console.log(fighter1)
+        console.log(fighter2)
+    }else{
         alert("You missed")}
     checkHealth()
 }
@@ -125,6 +140,8 @@ const fighter1restore = () => {
     checkHealth()
 }
 
+
+//create functions for play two actions 
 const fighter2Attack = () => {
     if (Math.random(fighter2.accuracy)<=0.8){
         fighter1.health = fighter1.health - 15
@@ -145,3 +162,37 @@ const fighter2restore = () => {
     fighter2.health=fighter2.health+15
     checkHealth()
 }
+//Event listners for actions 
+player1Attack.addEventListener("click", function () {
+    fighter1Attack(fighter1, fighter2)
+    }
+)
+player1Special.addEventListener("click", function () {
+    fighter1Special(fighter1, fighter2)
+    }
+)
+
+player1Restore.addEventListener("click", function () {
+    fighter1restore(fighter1, fighter2)
+    }
+)
+
+player1Attack.addEventListener("click", function () {
+    fighter1Attack(fighter1, fighter2)}
+)
+
+player1Special.addEventListener("click", function () {
+    fighter1Special(fighter1, fighter2)}
+)
+        
+player1Restore.addEventListener("click", function () {
+    fighter1restore(fighter1, fighter2)}
+)
+
+//Animation of images - adjust to sprites
+setInterval(() => {
+    const x = Math.floor(Math.random()* 10)
+    const y = Math.floor(Math.random() * -4)
+    fighter1pic.style.transform = `translate(${x}px,${y}px)`;
+    fighter2pic.style.transform = `translate(${x}px,${y}px)`;
+},1000)
