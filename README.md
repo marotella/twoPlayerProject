d7833e6c166750643491e51a7e7ffc655d89d6eb
marotella <111547566+marotella@users.noreply.github.com>
2023-01-31 22:48:33 -0500

    DOM and Audio

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # twoPlayerProject
 Two player fighting game 
+
+Sources: Sounds from mixkit.co
+Martial arts fast punch
+Indoor explosion
+Video Game health recharge
+Ominous drums
+Game Bonus Reached
--- js/app.js
+++ js/app.js
@@ -16,10 +16,12 @@ let fighter2;
 
 //create functions that assign a class to the character
 function createPlayerTwo(name) {
+    audioSelect()
     fighter2 = new fighter(name)
     console.log(fighter2)
 }
 function createPlayerOne(name) {
+    audioSelect()
     fighter1 = new fighter(name)
     console.log(fighter1)
 
@@ -44,20 +46,20 @@ const fighter1JCVDButton = document.querySelector(".jcvdButton1")
 console.log(fighter1JCVDButton)
 fighter1JCVDButton.addEventListener("click", function (){
     createPlayerOne("Van Dang")
-    fighter1pic.setAttribute("src", "images/bloodsport.jpeg")
+    fighter1pic.setAttribute("src", "images/vanDammeAttack.png")
 })
 const fighter1CommandoButton = document.querySelector(".arnoldButton1")
 console.log(fighter1CommandoButton)
 fighter1CommandoButton.addEventListener("click",  function (){
     createPlayerOne("Commando")
-    fighter1pic.setAttribute("src", "images/arnold.png")
+    fighter1pic.setAttribute("src", "images/arnoldAttack.png")
 })
 
 const fighter1RamboButton = document.querySelector(".ramboButton1")
 console.log(fighter1RamboButton)
 fighter1RamboButton.addEventListener("click", function (){
     createPlayerOne("Rambo")
-    fighter1pic.setAttribute("src", "images/RAmbo.png")
+    fighter1pic.setAttribute("src", "images/RamboAttack.png")
 })
 
 //Player Two
@@ -65,20 +67,20 @@ const fighter2JCVDButton = document.querySelector(".jcvdButton2")
 console.log(fighter2JCVDButton)
 fighter2JCVDButton.addEventListener("click", function (){
     createPlayerTwo("Van Dang")
-    fighter2pic.setAttribute("src", "images/bloodsport.jpeg")
+    fighter2pic.setAttribute("src", "images/vanDammeAttack.png")
 })
 
 const fighter2CommandoButton = document.querySelector(".arnoldButton2")
 console.log(fighter2CommandoButton)
 fighter2CommandoButton.addEventListener("click", function (){
     createPlayerTwo("Commando")
-    fighter2pic.setAttribute("src", "images/arnold.png")
+    fighter2pic.setAttribute("src", "images/arnoldAttack.png")
 })
 const fighter2RamboButton = document.querySelector(".ramboButton2")
 console.log(fighter2RamboButton)
 fighter2RamboButton.addEventListener("click",function (){
     createPlayerTwo("Bamro")
-    fighter2pic.setAttribute("src", "images/RAmbo.png")
+    fighter2pic.setAttribute("src", "images/RamboAttack.png")
 })
 
 
@@ -99,6 +101,13 @@ const checkHealth = () => {
         player1health.setAttribute("value", fighter1.health)
         player2health.setAttribute("value", fighter2.health)
         counter = counter + 1
+        if(counter % 2 == 0){
+            turnTracker.innerHTML = "It is Player 1's turn!"
+    
+        
+        } else if(counter% 2 != 0){
+            turnTracker.innerHTML = "It is Player 2's turn!"
+        }
     }
 }
 //Monitors turn taking should post to innerHTML the turn and start on click
@@ -106,20 +115,26 @@ const gameStart = document.querySelector(".fight")
 const turnTracker = document.querySelector(".turntracker")
 const body = document.querySelector("body")
 
-const gameTurns = () =>{
-
-    if(counter % 2 == 0){
-        turnTracker.innerHTML = "It is Player 1's turn!"
-
-    
-    } else{
-        turnTracker.innerHTML = "It is Player 2's turn!"
-
-     
+const initaiateGame = () =>{
+        audiofight()
+        var x = document.querySelector(".characterchoice");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
     }
+const showGameBoard = () => {
+      var y = document.querySelector(".arena");
+        if (y.style.display === "block") {
+          y.style.display = "none";
+        } else {
+          y.style.display = "block";
+        }
 }
-gameStart.addEventListener("click", gameTurns)
-
+      
+gameStart.addEventListener("click", initaiateGame)
+gameStart.addEventListener("click", showGameBoard)
 
 //create functions for fighting options and event listeners
 
@@ -128,41 +143,49 @@ gameStart.addEventListener("click", gameTurns)
 const fighter1Attack = () => {
     if (Math.random(fighter1.accuracy)<=0.8){
         if(fighter2.health > 14){
+            audioHit()
             fighter2.health = fighter2.health - 15
             console.log(fighter1)
             console.log(fighter2)
         }
         if(fighter2.health <= 14){
+            audioHit()
             fighter2.health = 0
             console.log(fighter1)
             console.log(fighter2)
         }
     }else {
-        alert("You missed")}
+        alert("You missed")
+        miss.play()}
     checkHealth()
 }
 
 const fighter1Special = () => {
     if (Math.random(fighter1.accuracy)<=0.5){
         if(fighter2.health > 29){
+            audioSpecial()
             fighter2.health = fighter2.health - 30
             console.log(fighter1)
             console.log(fighter2)
         }
         else if(fighter2.health <= 29){
             fighter2.health = 0
+            audioSpecial()
             console.log(fighter1)
             console.log(fighter2)
         }
     }else {
-        alert("You missed")}
+        alert("You missed")
+        miss.play()}
     checkHealth()
 }
 
 const fighter1restore = () => {
     if(fighter1.health > 95){
+        audioRestore()
         fighter1.health = 100
     } else if(fighter1.health <= 95){
+        audioRestore()
         fighter1.health=fighter1.health +15
     }
     checkHealth()
@@ -173,42 +196,55 @@ const fighter1restore = () => {
 const fighter2Attack = () => {
     if (Math.random(fighter2.accuracy)<=0.8){
         if(fighter1.health > 14){
+            audioHit()
             fighter1.health = fighter1.health - 15
             console.log(fighter1)
             console.log(fighter2)
         }
         else if(fighter1.health <= 14){
+            audioHit()
             fighter1.health = 0
             console.log(fighter1)
             console.log(fighter2)
         }
     }else{
-        alert("You missed")}
+        alert("You missed")
+        miss.play()}
     checkHealth()
 }
 
 const fighter2Special = () => {
     if (Math.random(fighter2.accuracy)<=0.5){
         if(fighter1.health > 29){
+            audioSpecial()
             fighter1.health = fighter1.health - 30
             console.log(fighter1)
             console.log(fighter2)
         }
         else if(fighter1.health <= 29){
             fighter1.health = 0
+            audioSpecial()
             console.log(fighter1)
             console.log(fighter2)
         }
     }else {
-        alert("You missed")}
+        alert("You missed")
+        miss.play()}
     checkHealth()
 }
 
 
 const fighter2restore = () => {
-    fighter2.health=fighter2.health+15
+    if(fighter2.health > 95){
+        audioRestore()
+        fighter2.health = 100
+    } else if(fighter2.health <= 95){
+        audioRestore()
+        fighter2.health=fighter2.health +15
+    }
     checkHealth()
 }
+
 //Event listners for actions 
 player1Attack.addEventListener("click", function () {
     fighter1Attack(fighter1, fighter2)
@@ -236,10 +272,46 @@ player2Restore.addEventListener("click", function () {
     fighter2restore(fighter1, fighter2)}
 )
 
-//Animation of images - adjust to sprites
+//Animation of images 
 setInterval(() => {
     const x = Math.floor(Math.random()* 10)
     const y = Math.floor(Math.random() * -4)
     fighter1pic.style.transform = `translate(${x}px,${y}px)`;
     fighter2pic.style.transform = `translate(${x}px,${y}px)`;
 },1000)
+
+//Sounds hit, death, restore, win, miss, start
+
+function audioHit() {
+    const hit = new Audio("https://assets.mixkit.co/sfx/download/mixkit-martial-arts-fast-punch-2047.wav")
+    hit.play()
+}
+
+function audioSpecial() {
+    const special = new Audio("https://assets.mixkit.co/sfx/download/mixkit-low-explosion-indoors-2187.wav")
+    special.play()
+}
+
+function audiofight(){
+    const fight = new Audio("https://assets.mixkit.co/sfx/download/mixkit-ominous-drums-227.wav")
+    fight.play()
+}
+
+function audioRestore() {
+    const restore = new Audio("https://assets.mixkit.co/sfx/download/mixkit-video-game-health-recharge-2837.wav")
+    restore.play()
+}
+function audioSelect() {
+    const select = new Audio("https://assets.mixkit.co/sfx/download/mixkit-arcade-retro-changing-tab-206.wav")
+    select.play()
+}
+
+function audioMiss() {
+    const miss = new Audio("https://assets.mixkit.co/sfx/download/mixkit-neutral-bot-pinbal-tone-3137.wav")
+    miss.play()
+}
+
+function audioWin() {
+    const win = new Audio("https://assets.mixkit.co/sfx/download/mixkit-game-bonus-reached-2065.wav")
+    win.play()
+}
